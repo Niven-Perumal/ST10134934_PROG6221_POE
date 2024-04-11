cd75442ff571976b44db31a9a1b8f0182960a2bb
Niven-Perumal <nivenperumalvc@gmail.com>
2024-04-12 00:40:33 +0200

    Creation of recipe class to be used

---
+++ Recipe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10134934_PROG6221_PartOne
+{
+     class Recipe
+    {
+//recipe class here
+
+    }
+}
