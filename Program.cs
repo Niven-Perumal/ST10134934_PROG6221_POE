e6152e266c6b34ceebae94ea80e620eeb299c1a6
Niven-Perumal <nivenperumalvc@gmail.com>
2024-04-14 16:32:34 +0200

    creating menu for the user to interact with and calling methods for the recipe app to function

--- Program.cs
+++ Program.cs
@@ -27,7 +27,74 @@ namespace ST10134934_PROG6221_PartOne
             //object to call methods from Recipe.cs
             Recipe recipe = new Recipe();
 
-          
+            // this loop will ensure the app runs until exit is selected
+            while (UserOp != 6)
+            {
+                try
+                {
+
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("Welcome to Recipe Tracker Application");
+                    Console.WriteLine("Please select an operation");
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("1 - Add Recipe");
+                    Console.WriteLine("2 - View Recipe");
+                    Console.WriteLine("3 - Scale Recipe");
+                    Console.WriteLine("4 - Rest Ingredients to original amount");
+                    Console.WriteLine("5 - Delete Recipe");
+                    Console.WriteLine("6 - Exit program");
+                    Console.WriteLine("----------------------------------------");
+
+
+                    userOp = Console.ReadLine();
+                    UserOp = int.Parse(userOp);
+
+                    //this switch case calls all methods from Recipe.cs (method class) that will run based on operation selected by user
+                    switch (UserOp)
+                    {
+
+                        case 1:
+                            recipe.addRecipe(recipeName, ingName, ingQuan, unitMea, userIngAmt, ingNum, recipeNameArrayList, ingNameArrayList, ingQuanArrayList, ingUnitArrayList);
+                            recipe.stepsRecipe(userIngAmt, ingNum, stepArrayList, recipeNameArrayList);
+                            break;
+                        case 2:
+                            recipe.viewRecipe(scaledIngQuanArrayList, sumOne, userIngAmt, ingNum, recipeNameArrayList, ingNameArrayList, ingQuanArrayList, ingUnitArrayList, stepArrayList);
+                            break;
+                        case 3:
+                            recipe.scaleRecipe(scaledIngQuanArrayList, sumOne, recipeNameArrayList, ingNameArrayList, ingQuanArrayList, ingUnitArrayList);
+                            break;
+                        case 4:
+                            recipe.resetRecipe(scaledIngQuanArrayList, ingName, ingQuan, unitMea, userIngAmt, ingNum, ingNameArrayList, ingQuanArrayList, ingUnitArrayList, stepArrayList);
+                            break;
+                        case 5:
+                            recipe.deleteRecipe(scaledIngQuanArrayList, recipeNameArrayList, ingNameArrayList, ingQuanArrayList, ingUnitArrayList, stepArrayList);
+                            break;
+                        case 6:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("----------------------------------------");
+                            Console.WriteLine("Exit program selected goodbye!");
+                            Console.WriteLine("----------------------------------------");
+                            Console.ResetColor();
+                            System.Environment.Exit(0);
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Enter a valid operation");
+                            Console.ResetColor();
+                            break;
+                    }
+
+
+
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invaild operation selected");
+                    Console.WriteLine("----------------------------------------");
+                }
+
+            }
 
 
 
